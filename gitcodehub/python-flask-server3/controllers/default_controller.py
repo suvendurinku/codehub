0654bc9b40d735fbcd6b37ac263197e33fb33fea
Suvendu Barik <suvendu.barik@comparethemarket.com>
2016-09-27 16:24:59 +0100

    Add files via upload

---
+++ gitcodehub/python-flask-server3/controllers/default_controller.py
@@ -0,0 +1,21 @@
+
+def delete_sale(saleId, ctmVisitorId, ctmSessionId, ctmCorrelationId = None, ctmCausationId = None) -> str:
+    return 'do some magic!'
+
+def ping() -> str:
+    return 'do some magic!'
+
+def sales_by_account_id(accountId, ctmVisitorId, ctmSessionId, productCode = None, ctmCorrelationId = None, ctmCausationId = None) -> str:
+    return 'do some magic!'
+
+def sales_by_email(email, ctmVisitorId, ctmSessionId, productCode = None, ctmCorrelationId = None, ctmCausationId = None) -> str:
+    return 'do some magic!'
+
+def sales_by_enquiry_id(enquiryId, ctmVisitorId, ctmSessionId, ctmCorrelationId = None, ctmCausationId = None) -> str:
+    return 'do some magic!'
+
+def store_sale(salePayload, ctmVisitorId, ctmSessionId, ctmCorrelationId = None, ctmCausationId = None) -> str:
+    return 'do some magic!'
+
+def store_sales(salesPayload, ctmVisitorId, ctmSessionId, ctmCorrelationId = None, ctmCausationId = None) -> str:
+    return 'do some magic!'
